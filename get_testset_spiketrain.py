bb77e790ead5c3456e8c4a040bcd1ec9af1ead95
codethekitty <36994992+codethekitty@users.noreply.github.com>
2022-02-14 18:56:47 -0500

    fixed t (?)

--- get_testset_spiketrain.py
+++ get_testset_spiketrain.py
@@ -7,10 +7,12 @@ flist=glob.glob('CW_GP_*')
 for f in flist:
     dataset=[]
     tlist=glob.glob(os.path.join(f,'spike_detect','rec*'))
+    t0=load(os.path.join(f,'FRA.npy'),allow_pickle=True)[1]['start']
     for t in tlist:
         sp=load(t,allow_pickle=True)[0]
         for k,v in sp.items():
-            dataset.append({'f':int(f[6:]),'t':int(t[-8:-4]),'ch':k,'ts':v})
+            t_norm=int(t[-8:-6])*60+int(t[-6:-4])-t0
+            dataset.append({'f':int(f[6:]),'t':t_norm,'ch':k,'ts':v})
         print(f,t)
     save('test_set_220211/spiketrain_'+f[6:]+'.npy',dataset)
     print(sys.getsizeof(dataset))
